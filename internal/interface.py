74f3cd6a4c9a04bf53440275c3ed89a0d913569f
alyoshaslab <139561770+ohfkgsofsex@users.noreply.github.com>
2025-12-22 00:04:02 +0300

    Test app debloat

--- internal/interface.py
+++ internal/interface.py
@@ -1,33 +1,13 @@
-from PySide6.QtCore import Qt, QEvent, QUrl
-from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt, QEvent
 from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-from PySide6.QtMultimedia import QSoundEffect
 from .resources import get_resource
-import os, base64
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.resize(250, 250)
-        self.setWindowTitle("fear me")
-
-        self.layout = QVBoxLayout(self)
-        self.setLayout(self.layout)
-
-        self.label = QLabel(self)
-        pmap = QPixmap()
-        pmap.load(get_resource("background.png"))
-        self.label.setPixmap(pmap)
-        self.label.setScaledContents(True)
-        self.label.resize(self.size())
-        self.label.lower()
-        self.layout.addWidget(self.label)
-
-        self.sound = QSoundEffect(self)
-        self.sound.setSource(QUrl.fromLocalFile(get_resource("sound.wav")))
-
-    def closeEvent(self, event: QEvent):
-        self.sound.stop()
-        self.sound.play()
-        event.ignore()
+        self.resize(600, 300)
+        self.setWindowTitle("Addon Manager")
+        self.setStyleSheet("""
+            QWidget { background-color: #1e1e1e; color: #dcdcdc; }
+            """)
